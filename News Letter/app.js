78c5673c9801c369dddbb1dc66fd992c4453a5d3
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-15 17:33:36 +0530

    set up the signup page and taking inputs

--- News Letter/app.js
+++ News Letter/app.js
@@ -6,8 +6,18 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var emailAddress = req.body.emailAddress;
+
+  console.log(firstName, lastName, emailAddress);
 });
 
 app.listen(3000, function () {
