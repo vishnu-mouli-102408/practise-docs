441009ff5bb2c475207cd7197179294c79b9204b
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-17 11:30:58 +0530

    some code practise about weekdays and days

--- Todo-V1/app.js
+++ Todo-V1/app.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  var today = new Date();
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.send("<h1>It's Saturday or Sunday, baby!</h1>");
+  } else {
+    res.send("<h1>It's Working day, baby!</h1>");
+  }
 });
 
 app.listen(3000, function () {
