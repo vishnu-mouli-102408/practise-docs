5c063e93ee56e6f7525f848881d6d8a92c5fb9bf
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-20 20:06:32 +0530

    Inserted multiple data into the mongodb server using director model

--- Mdb-Mongoose-Movies/app.js
+++ Mdb-Mongoose-Movies/app.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 // connecting mongoose and mongoDB server
-mongoose.connect("mongodb://127.0.0.1:27017/moviesDB");
+mongoose.connect("mongodb://127.0.0.1:27017/moviesDB", {
+  useNewUrlParser: true,
+});
 
 // Step-1 Creating the new Schema
 const movieSchema = new mongoose.Schema({
@@ -34,12 +36,36 @@ const directorSchema = new mongoose.Schema({
 
 const Director = mongoose.model("director", directorSchema);
 
-// Inserting the data into the model
+// Inserting single the data into the model
 
-const director = new Director({
-  name: "Christopher Nolan",
-  movie: "Oppenheimer",
-  age: 67,
+// director = new Director({
+//   name: "Mouli",
+//   movie: "Fast X",
+//   age: 22,
+// });
+
+// director.save();
+
+// Inserting Multiple data into the model
+
+const director1 = new Director({
+  name: "Rahul",
+  movie: "John Wick",
+  age: 33,
 });
 
-director.save();
+const director2 = new Director({
+  name: "Mouli",
+  movie: "Fast X",
+  age: 22,
+});
+
+const defaultItem = [director1, director2];
+
+Director.insertMany(defaultItem)
+  .then(function () {
+    console.log("Successfully saved default items to DB");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
