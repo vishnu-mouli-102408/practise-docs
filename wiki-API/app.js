ce849e6ac5a7124b611ec53589ae2e6a6a13c668
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-25 09:02:34 +0530

    Setting up the mongoose database server

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -11,6 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+Article = mongoose.model("Article", articleSchema);
+
 app.listen(3000, function () {
   console.log("Server Started running on port 3000");
 });
