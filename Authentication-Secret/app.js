09d56d91ad36cbd02fbda5eacc89bd3d47def6ae
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-25 21:53:22 +0530

    Using environment ariables to keep secrets safe

--- Authentication-Secret/app.js
+++ Authentication-Secret/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,9 +23,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
