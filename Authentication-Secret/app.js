dc2f9d353ff917c0a0c5222c467c9b8dc68babd9
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-27 18:34:22 +0530

    Secured session secret for google auth using environmental variables

--- Authentication-Secret/app.js
+++ Authentication-Secret/app.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 app.use(
   session({
-    secret: "This is my little secret.",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
